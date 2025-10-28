744b8247686952e9104709a51ffcff6308a85c7a
koki-maekawa <koki.study33@gmail.com>
2025-10-28 03:01:13 +0000

    #24 fix: 多重階層に対してlintできるように修正

--- format_erb.sh
+++ format_erb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 echo "Running HTML Beautifier..."
-bundle exec htmlbeautifier app/views/**/*.html.erb
+bundle exec htmlbeautifier app/views/*/*.html.erb
+bundle exec htmlbeautifier app/views/*/*/*.html.erb
 
 echo "Running ERB Lint..."
-bundle exec erb_lint --autocorrect app/views/**/*.html.erb
+bundle exec erb_lint --autocorrect app/views/*/*.html.erb
+bundle exec erb_lint --autocorrect app/views/*/*/*.html.erb
