37c595960c0ec4fdd63140acc2b7be902649c855
koki-maekawa <koki.study33@gmail.com>
2025-10-30 04:43:59 +0000

    #28 feat: おにぎり画像をレコードに添付する

--- app/models/rice_ball.rb
+++ app/models/rice_ball.rb
@@ -2,6 +2,10 @@ class RiceBall < ApplicationRecord
   belongs_to :user
   has_many :ingredients, dependent: :destroy
   has_many :steps, dependent: :destroy
+  has_one_attached :image do |attachable|
+    attachable.variant :index, resize_and_pad: [ 500, 500 ], preprocessed: true
+    attachable.variant :show, resize_and_pad: [ 1000, 1000 ], preprocessed: true
+  end
 
   def self.ransackable_attributes(auth_object = nil)
     [ "title", "created_at", "updated_at", "user_id" ]
