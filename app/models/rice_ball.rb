d7bb2d5ba13bfca7901d70e81da15c12583c38e3
koki-maekawa <koki.study33@gmail.com>
2025-10-30 04:41:26 +0000

    #28 feat: ransack承認

--- app/models/rice_ball.rb
+++ app/models/rice_ball.rb
@@ -2,4 +2,12 @@ class RiceBall < ApplicationRecord
   belongs_to :user
   has_many :ingredients, dependent: :destroy
   has_many :steps, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "title", "created_at", "updated_at", "user_id" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "ingredients", "user" ]
+  end
 end
