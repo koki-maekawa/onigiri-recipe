97ae9baa72e8e5094a421d2efaff9853b5e0cf65
koki-maekawa <koki.study33@gmail.com>
2025-11-08 11:23:26 +0000

    #41 feat: bookmarkモデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_30_043817) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_08_112215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_30_043817) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "rice_ball_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rice_ball_id"], name: "index_bookmarks_on_rice_ball_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -86,6 +95,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_30_043817) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "rice_balls"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "ingredients", "rice_balls"
   add_foreign_key "rice_balls", "users"
   add_foreign_key "steps", "rice_balls"
