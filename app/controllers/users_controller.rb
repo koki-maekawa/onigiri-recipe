85a76fc41f01cd0f525376ca256472f0562c500c
koki-maekawa <koki.study33@gmail.com>
2025-11-09 11:27:45 +0000

    #41 feat: ユーザー詳細画面関連コントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show created_index bookmarked_index]
+
+  def show
+    @created_rice_balls = @user.rice_balls.includes(:image_attachment).order(created_at: :desc).limit(4)
+    @created_rice_balls_count = @user.rice_balls.size
+    @bookmarked_rice_balls = @user.bookmarked_rice_balls.includes(:image_attachment, :user).order("bookmarks.created_at DESC").limit(4)
+    @bookmarked_rice_balls_count = @user.bookmarked_rice_balls.size
+  end
+
+  def created_index
+    @created_rice_balls = @user.rice_balls.includes(:image_attachment).order(created_at: :desc).page(params[:page]).per(12)
+  end
+
+  def bookmarked_index
+    @bookmarked_rice_balls = @user.bookmarked_rice_balls.includes(:image_attachment, :user).order("bookmarks.created_at DESC").page(params[:page]).per(12)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
